274ac95d428bef8ee60d1bd6704563b9256f0d0b
Rakshit <98008663+Rakshit-CS-AIML@users.noreply.github.com>
2023-05-20 18:50:30 +0530

    Add files via upload

---
+++ GCD_FIB/gcd.java
@@ -0,0 +1,13 @@
+public class gcd {
+    public static void main(String[] args) {
+		int divisor = 36;
+		int divident = 60;
+
+		while (divident % divisor != 0) {
+			int rem = divident % divisor;
+			divident = divisor;
+			divisor = rem;
+		}
+		System.out.println(divisor);
+	}
+}
